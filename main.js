efe7b91713cba2b42668f45ea635867cef5cc094
Austin Sorrell <93938711+AustinDSorrell@users.noreply.github.com>
2022-04-19 08:44:20 -0700

    classified commit : Code (BALLS), Object Class KETER

--- main.js
+++ main.js
@@ -12,3 +12,85 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+function Ball (x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10,20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+  requestAnimationFrame(loop);
+}
+
+Ball.prototype.collsionDetect = function() {
+  for (let i = 0; i < balls.length; i++) {
+    if (!(this === balls[i])) {
+      const dx = this.x - balls [i].x;
+      const dy = this.y - balls [i].y;
+      const distance = Math.sqrt(dx*dx+dy*dy);
+
+      if (distance < this.size + balls[i].size) {
+        balls[i].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0,255) + ',' + random(0,255) +')';
+      }
+    }
+  }
+}
+
+
+
